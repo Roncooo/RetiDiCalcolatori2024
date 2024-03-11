e8ff294a94564f0a2cf1d98c6153903ef0bd813b
Francesco <francesco.roncolato@studenti.unipd.it>
2024-03-11 10:50:32 +0100

    ok finito, non sono riuscito (non so se si riesca) a trovare l'inizio dello heap

--- es01_stack_heap.c
+++ es01_stack_heap.c
@@ -5,57 +5,107 @@
  *  - direzione di crescita e posizione di
  *     - heap
  *     - stack
-*/
+ */
 
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdio.h>
 
+short int global1 = 2;
+short int global2 = 3;
+short int uglobal1 = 4;
+short int uglobal2 = 5;
 
+void func(int i);
+void* fine_heap();
 
+void main()
+{
 
-/**
- * This function lets you convert a variable to 
- * a string with its binary representation.
- * To indicate the variable you need to provide
- *  - the pointer to its location
- *  - the size in bytes
- * The function returns a pointer to a c-style string
- * that can be printed with printf("%s", str);
- * 
- * Usage:
- *      uint16_t i = 0xF0F0;
- *      char* str = binaryString(&i, sizeof(i));
- *      printf("%s", str);
- *      free(str);
- * 
-*/
-uint8_t* binaryString(void* ptr, uint16_t nbytes){
-    uint8_t* p = (uint8_t*)ptr;
-    uint32_t nbit = 8*nbytes;
-    uint8_t* str = malloc(nbit+1);
-    uint8_t mask;
-    for(int16_t i=0; i<nbytes; i++){
-        mask = 0x01;
-        for(uint32_t j=0; j<8; j++){
-            if( (p[i]&mask) == 0)
-                str[i*8+j] = '0';
-            else
-                str[i*8+j] = '1';
-            
-            mask = mask << 1;
-        }
+    short int i1 = 0x0001;
+    short int i2 = 0x0001;
+
+    // char* str = binaryString(&i, sizeof(i));
+    // printf("%s\n",str);
+    // free(str);
+
+    printf("\n%s\n", "AREA DELLO STACK");
+    printf("0x%lx \t: %s\n", &i1, "prima variabile dello stack del main");
+    printf("0x%lx \t: %s\n", &i2, "seconda variabile dello stack del main");
+
+    func(3);
+
+    printf("\n%s\n", "AREA DI MEMORIA DINAMICA (non è l'inizio dello heap, carica dove vuole lui)");
+    void *ptr1 = malloc(256);
+    printf("0x%lx \t: %s\n", ptr1, "inizio della prima area dinamica allocata dal main");
+
+    void *temp_ptr = realloc(ptr1, 257);
+
+    void *ptr2 = malloc(256);
+    printf("0x%lx \t: %s\n", ptr2, "inizio della seconda area dinamica allocata dal main");
+    printf("%s\n", "Rialloco la prima area con una dimensione uguale a quella di prima");
+    void *temp = realloc(ptr1, 256);
+    if (temp == NULL)
+    {
+        printf("\n%s\n", "Allocation failure");
+        free(ptr1);
+        free(ptr2);
+        return;
     }
-    str[nbit] = 0;    // terminator
-    return str;
+    ptr1 = temp;    // allocation succeded, continua ad usare solo ptr1
+    printf("0x%lx \t: %s\n", ptr1, "puntatore riallocato");
+
+    printf("%s\n", "Rialloco la prima area con una dimensione maggiore");
+    temp = realloc(ptr1, 304);
+    if (temp == NULL)
+    {
+        printf("\n%s\n", "Allocation failure");
+        free(ptr1);
+        free(ptr2);
+        return;
+    }
+    ptr1 = temp;
+    printf("0x%lx \t: %s\n", ptr1, "puntatore riallocato");
+
+    free(ptr1);
+    free(ptr2);
+
+    printf("\n%s\n", "AREA DI MEMORIA STATICA");
+    printf("0x%lx \t: %s\n", &uglobal1, "prima variabile globale non inizializzata");
+    printf("0x%lx \t: %s\n", &uglobal2, "seconda variabile globale non inizializzata");
+    printf("0x%lx \t: %s\n", &global1, "prima variabile globale inizializzata");
+    printf("0x%lx \t: %s\n", &global2, "seconda variabile globale inizializzata");
+
+    printf("\n%s\n", "AREA CODICE");
+    printf("0x%lx \t: %s\n", main, "puntatore a main");
+    printf("0x%lx \t: %s\n", func, "puntatore a func");
+}   
+
+void func(int i)
+{
+    if (i < 0)
+        return;
+    short int x = 2;
+
+    // printf("0x%lx \t: %s\n", &i, "indirizzo del parametro di func");
+    printf("0x%lx \t: %s %d %s\n", &x, "variabile dello stack della chiamata", i, "di func");
+    func(i - 1);
 }
 
 
-void main(){
+// ogni tanto fa esplodere windows, meglio non chiamarla
+void* fine_heap(){
 
-    short int i = 0x1001;
-    char* str = binaryString(&i, sizeof(i));
-    printf("%s\n",str);
-    free(str);
+    void* ptr1;
+    void* ptr2;
+
+    while(1)
+    {
+        ptr1 = malloc(1024*1024);
+        if(ptr1 == NULL)
+            return ptr2;
+        ptr2 = ptr1;
+        // free(ptr1);
+    }
 
 }
