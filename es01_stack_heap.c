bae5be5b5c61683ab308fbeb59d2964d1da57d34
Francesco <francesco.roncolato@studenti.unipd.it>
2024-03-19 17:22:23 +0100

    esercizi svolti in aula

--- es01_stack_heap.c
+++ es01_stack_heap.c
@@ -30,19 +30,19 @@ void main()
     // free(str);
 
     printf("\n%s\n", "AREA DELLO STACK");
-    printf("0x%lx \t: %s\n", &i1, "prima variabile dello stack del main");
-    printf("0x%lx \t: %s\n", &i2, "seconda variabile dello stack del main");
+    printf("0x%p \t: %s\n", &i1, "prima variabile dello stack del main");
+    printf("0x%p \t: %s\n", &i2, "seconda variabile dello stack del main");
 
     func(3);
 
     printf("\n%s\n", "AREA DI MEMORIA DINAMICA (non è l'inizio dello heap, carica dove vuole lui)");
     void *ptr1 = malloc(256);
-    printf("0x%lx \t: %s\n", ptr1, "inizio della prima area dinamica allocata dal main");
+    printf("0x%p \t: %s\n", ptr1, "inizio della prima area dinamica allocata dal main");
 
     void *temp_ptr = realloc(ptr1, 257);
 
     void *ptr2 = malloc(256);
-    printf("0x%lx \t: %s\n", ptr2, "inizio della seconda area dinamica allocata dal main");
+    printf("0x%p \t: %s\n", ptr2, "inizio della seconda area dinamica allocata dal main");
     printf("%s\n", "Rialloco la prima area con una dimensione uguale a quella di prima");
     void *temp = realloc(ptr1, 256);
     if (temp == NULL)
@@ -53,7 +53,7 @@ void main()
         return;
     }
     ptr1 = temp;    // allocation succeded, continua ad usare solo ptr1
-    printf("0x%lx \t: %s\n", ptr1, "puntatore riallocato");
+    printf("0x%p \t: %s\n", ptr1, "puntatore riallocato");
 
     printf("%s\n", "Rialloco la prima area con una dimensione maggiore");
     temp = realloc(ptr1, 304);
@@ -65,20 +65,20 @@ void main()
         return;
     }
     ptr1 = temp;
-    printf("0x%lx \t: %s\n", ptr1, "puntatore riallocato");
+    printf("0x%p \t: %s\n", ptr1, "puntatore riallocato");
 
     free(ptr1);
     free(ptr2);
 
     printf("\n%s\n", "AREA DI MEMORIA STATICA");
-    printf("0x%lx \t: %s\n", &uglobal1, "prima variabile globale non inizializzata");
-    printf("0x%lx \t: %s\n", &uglobal2, "seconda variabile globale non inizializzata");
-    printf("0x%lx \t: %s\n", &global1, "prima variabile globale inizializzata");
-    printf("0x%lx \t: %s\n", &global2, "seconda variabile globale inizializzata");
+    printf("0x%p \t: %s\n", &uglobal1, "prima variabile globale non inizializzata");
+    printf("0x%p \t: %s\n", &uglobal2, "seconda variabile globale non inizializzata");
+    printf("0x%p \t: %s\n", &global1, "prima variabile globale inizializzata");
+    printf("0x%p \t: %s\n", &global2, "seconda variabile globale inizializzata");
 
     printf("\n%s\n", "AREA CODICE");
-    printf("0x%lx \t: %s\n", main, "puntatore a main");
-    printf("0x%lx \t: %s\n", func, "puntatore a func");
+    printf("0x%p \t: %s\n", main, "puntatore a main");
+    printf("0x%p \t: %s\n", func, "puntatore a func");
 }   
 
 void func(int i)
@@ -87,8 +87,8 @@ void func(int i)
         return;
     short int x = 2;
 
-    // printf("0x%lx \t: %s\n", &i, "indirizzo del parametro di func");
-    printf("0x%lx \t: %s %d %s\n", &x, "variabile dello stack della chiamata", i, "di func");
+    // printf("0x%p \t: %s\n", &i, "indirizzo del parametro di func");
+    printf("0x%p \t: %s %d %s\n", &x, "variabile dello stack della chiamata", i, "di func");
     func(i - 1);
 }
 
